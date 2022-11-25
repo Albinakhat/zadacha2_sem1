6efffdb72217f996568dd9c15eeeaace81b0d9b6
Albinakhat <albilove@inbox.ru>
2022-11-25 10:37:54 +0300

    исправили ошибки

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Напишите программу, которая на вход принимает три числа и выдает максимальное из этих чисел
+// 2,3,7, -> 7
+// 44 5 78 -> 78
+// 22 3 9 ->22
+Console.Clear();
+Console.WriteLine("ВВедите число");
+Console.WriteLine("a=");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("b=");
+int b= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("c=");
+int c= Convert.ToInt32(Console.ReadLine());
+if (a<b)
